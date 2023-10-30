6c786c85ea83ba16ee2d06299d764d3b5af773d7
renandsz <renandsz@hotmail.com>
2023-10-29 22:12:11 -0300

    select

--- UE Game/Assets/Scripts/GameManager.cs
+++ UE Game/Assets/Scripts/GameManager.cs
@@ -1,18 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+
+    private AudioSource _audioSource;
+    public AudioMixer audioMixer;
+    public AudioClip selectUI;
+
+    private void Awake()
     {
+        if (instance != null)
+        {
+            instance = this;
+            Inicializacao();
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Inicializacao()
     {
+        TryGetComponent(out _audioSource);
+    }
+
+    public void SelectUI()
+    {
+        _audioSource.PlayOneShot(selectUI);
+    }
+    public void MutarAudio(bool mudo)
+    {
+        if (mudo)
+        {
+            //variavel exposta no audiomixer grupo master
+            //o nome padrao da variavel era MyExposedParam e foi alterado pra Volume
+            audioMixer.SetFloat("Volume", -80); //mudo
+        }
+        else
+        {
+            audioMixer.SetFloat("Volume", 0); //nao-mudo
+        }
         
     }
+
 }

c0c8d211f924465ddb6699c78e67be03784d5840
Renan Santos <renandsz@hotmail.com>
2023-10-30 12:08:38 -0300

    PlayerOK

---
+++ UE Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    public int velocidade = 10;
+    public int forcaPulo = 7;
+    private Rigidbody _rb;
+    public bool noChao;
+    private Transform cameraPivot;
+    private Animator _animator;
+
+    private bool _isMoving;
+    void Start()
+    {
+        TryGetComponent(out _animator);
+        TryGetComponent(out _rb);
+        cameraPivot = Camera.main.transform;
+    }
+    void OnCollisionEnter(Collision col) {
+        noChao = true;
+    }
+    void Update() {
+       Movimento();
+       Pulo();
+       AtualizarAnimator();
+       VerificarReinicio();
+    }
+
+    void Movimento()
+    {
+        float h = Input.GetAxis("Horizontal"); //-1 esquerda,0 nada, 1 direita
+        float v = Input.GetAxis("Vertical");// -1 pra tras, 0 nada, 1 pra frente
+        
+        Vector3 direcao = (cameraPivot.forward * v + cameraPivot.right * h) * velocidade;
+        direcao = new Vector3(direcao.x, _rb.velocity.y, direcao.z);
+        _rb.velocity = Vector3.Lerp(_rb.velocity, direcao, 5 * Time.deltaTime);
+
+        if (noChao && _rb.velocity != Vector3.zero)
+        {
+            _isMoving = true;
+        }
+        else if(noChao && _rb.velocity == Vector3.zero)
+        {
+            _isMoving = false;
+        }
+
+        if (_isMoving)
+        {
+            Vector3 cameraEuler = new Vector3(0, cameraPivot.eulerAngles.y, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation ,Quaternion.Euler(cameraEuler), 5 * Time.deltaTime );
+        }
+    }
+    void Pulo()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && noChao ) {
+            _rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse); //pulo
+            noChao = false;
+        }
+    }
+
+    void AtualizarAnimator()
+    {
+        _animator.SetBool("IsMoving",_isMoving);
+    }
+
+    void VerificarReinicio()
+    {
+        if(transform.position.y <= -10) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
